ab49d02c5dfe0188c31aaba4a9423caf1bfd11fc
Iivar <iivari.vanuden@ad.helsinki.fi>
2025-01-30 16:15:09 +0200

    Add Login/Register system and mainpage at '/'

--- app/app.py
+++ app/app.py
@@ -1,36 +1,57 @@
 import sqlite3
 from flask import Flask
-from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import db
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def user():
     return render_template("login.html")
 
-@app.route("/login")
-def loginpage():
-    return render_template("loginpage.html", message="log")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", message="")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-@app.route("/register")
-def register(error = ""):
-    return render_template("register.html", message = error)
+        sql_password = "SELECT password_hash FROM users WHERE username = ?"
+        password_hash = db.query(sql_password, [username])
 
-@app.route("/create", methods=["POST"])
-def create():
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
-    if password1 != password2:
-        return register(error = "VIRHE: salasanat eivät ole samat")
-    password_hash = generate_password_hash(password1)
+        if password_hash:
+            password_hash = password_hash[0][0]
+            if check_password_hash(password_hash, password):
+                session["username"] = username
+                return redirect("/")
+        return render_template("login.html", message="VIRHE: väärä tunnus tai salasana")
 
-    try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-    except sqlite3.IntegrityError:
-        return register(error = "VIRHE: tunnus on jo varattu")
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html", message = "")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("register.html", message = "VIRHE: salasanat eivät ole samat")
+        password_hash = generate_password_hash(password1)
 
-    return register(error = "Tunnus luotu")
+        try:
+            sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+            db.execute(sql, [username, password_hash])
+        except sqlite3.IntegrityError:
+            return render_template("register.html", message = "VIRHE: tunnus on jo varattu")
+
+        session["username"] = username
+        return redirect("/")
+    
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
