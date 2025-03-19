d0b81a84a7ab7a6e0d69fd6cf01cb41d8e1a84d6
Iivar <iivari.vanuden@ad.helsinki.fi>
2025-03-20 00:20:37 +0200

    fix effects showing in catalogue when secret

--- app/news.py
+++ app/news.py
@@ -5,7 +5,7 @@ Checkout the github: 'https://github.com/Mrivu/ForageTool'
 Contact us at: 'ivugames@outlook.fi'"""
 
 news = {
-    "Version": "1.0.1",
+    "Version": "1.0.2",
     "Release": "19.3.2025",
     "Changes": changes
 }
