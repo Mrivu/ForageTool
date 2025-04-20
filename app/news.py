365a0fbfc2affaa01e3b2412d061075273182f41
Iivari van Uden <110121897+Mrivu@users.noreply.github.com>
2025-04-20 15:20:31 +0300

    Update news.py

--- app/news.py
+++ app/news.py
@@ -2,7 +2,7 @@ changes = """Forage tool releases!
 We are working hard on the potion crafting system next.
 Eventually, we plan on adding a world system, allowing for custom plants and probability.
 Checkout the github: 'https://github.com/Mrivu/ForageTool'
-Contact us at: 'ivugames@outlook.fi'"""
+Contact us at: 'ivugames@outlook.com'"""
 
 news = {
     "Version": "1.0.2",
